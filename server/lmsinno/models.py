2afcfd658b5615fa388cece2a934b982225aa1a7
Artemii Bykov <theunsignedfairon@gmail.com>
2018-01-22 00:30:35 +0300

    Create class Document

--- server/lmsinno/models.py
+++ server/lmsinno/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+# 0 - book; 1 - journal article; 2 - AV
+TYPE_CHOICES = [(i, i) for i in range(2)]
+
+
+class Document(models.Model):
+    document_id = models.AutoField(primary_key=True, unique=True)
+    title = models.CharField(unique=True, max_length=100)
+    description = models.TextField(max_length=200)
+    publisher = models.CharField(max_length=100)
+    year = models.IntegerField()
+    type = models.IntegerField(choices=TYPE_CHOICES, default=0)
+    price = models.FloatField()
+    is_reference = models.BooleanField(default=False)
+    copies_available = models.IntegerField()
