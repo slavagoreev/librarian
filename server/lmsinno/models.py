88ac7d2add94bf6ba80a4842ffdb3b37b205766c
Artemii Bykov <theunsignedfairon@gmail.com>
2018-01-22 11:15:31 +0300

    Create Table of User

--- server/lmsinno/models.py
+++ server/lmsinno/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
-# 0 - book; 1 - journal article; 2 - AV
-TYPE_CHOICES = [(i, i) for i in range(2)]
+# Type of Documents:
+# 0 - Book; 1 - Journal article; 2 - AV
+DOCUMENT_TYPE_CHOICES = [(i, i) for i in range(2)]
+
+# Type of User:
+# 0 - basic user; 1 - Faculty; 2 - Librarian
+USER_TYPE_CHOICES = [(i, i) for i in range(2)]
 
 
 class Document(models.Model):
@@ -10,7 +15,19 @@ class Document(models.Model):
     description = models.TextField(max_length=200)
     publisher = models.CharField(max_length=100)
     year = models.IntegerField()
-    type = models.IntegerField(choices=TYPE_CHOICES, default=0)
+    type = models.IntegerField(choices=DOCUMENT_TYPE_CHOICES, default=0)
     price = models.FloatField()
     is_reference = models.BooleanField(default=False)
-    copies_available = models.IntegerField()
+    copies_available = models.IntegerField(default=0)
+
+
+class User(models.Model):
+    user_id = models.AutoField(primary_key=True, unique=True)
+    email = models.CharField(unique=True, max_length=50)
+    password = models.CharField(default=None, max_length=32)
+    password_salt = models.CharField(default=None, max_length=32)
+    role = models.IntegerField(default=0, choices=USER_TYPE_CHOICES)
+    first_name = models.CharField(max_length=20, default=None)
+    last_name = models.CharField(max_length=20, default=None)
+    address = models.CharField(max_length=100)
+    phone = models.IntegerField(unique=True)
