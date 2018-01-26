c1fcebb588187bbb1c6685b1e29071ce19b9a865
Pavel <32571227+indionapolis@users.noreply.github.com>
2018-01-26 19:46:51 +0300

    Update urls.py
    urls for new features

--- server/lmsinno/urls.py
+++ server/lmsinno/urls.py
@@ -5,6 +5,9 @@ from rest_framework.urlpatterns import format_suffix_patterns
 urlpatterns = [
     url(r'document/(?P<document_id>[0-9]+)/$', views.DocumentDetail.as_view()),
     url(r'document/$', views.DocumentsByCriteria.as_view())
+    
+    url(r'authorization/$', views.Authorization.as_view()),
+    url(r'registration/$', views.Registration.as_view()),
 ]
 
 format_suffix_patterns(urlpatterns)
