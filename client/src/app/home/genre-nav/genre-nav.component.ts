dfc09169c95050e0ccb027f1dd08aa7ba4adec11
slavagoreev <admin@reespace.ru>
2018-02-07 01:37:55 +0300

    Scroll menu Angular States

---
+++ client/src/app/home/genre-nav/genre-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-genre-nav',
+  templateUrl: './genre-nav.component.html',
+  styleUrls: ['./genre-nav.component.scss']
+})
+export class GenreNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
