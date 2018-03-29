1ba17d3b3a9b3fea088541fd7d89bbb5802b0b71
George Watson <alice4tests@gmail.com>
2018-03-30 02:59:30 +0300

    bot system v0.1

--- server/lmsinno/tg_bot/engine.py
+++ server/lmsinno/tg_bot/engine.py
@@ -2,5 +2,5 @@
 import requests as rq
 
 def send_message(chat_id, msg):
-    link = 'https://api.telegram.org/bot563324296:AAG6dtFZk9Sh3IG2_RIOJ5ltPrradgEOmEw/sendMessage?chat_id=' + chat_id + '&text=' + msg
+    link = 'https://api.telegram.org/bot563324296:AAG6dtFZk9Sh3IG2_RIOJ5ltPrradgEOmEw/sendMessage?chat_id=' + str(chat_id) + '&text=' + msg
     rq.get(link)
