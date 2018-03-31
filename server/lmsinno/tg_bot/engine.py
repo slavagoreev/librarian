61522601279e5f349a8e79078aa5de4f310156b7
indionapolis <panddromas@gmail.com>
2018-03-31 20:16:46 +0300

    bug fix

--- server/lmsinno/tg_bot/engine.py
+++ server/lmsinno/tg_bot/engine.py
@@ -4,7 +4,6 @@ from ..const import BOT_KEY
 
 
 def send_message(user, msg):
-    print(rq.get('https://oauth.telegram.org/auth/get?bot_id=563324296&origin=https%3A%2F%2Ftrainno.ru'))
     link = 'https://api.telegram.org/bot'+BOT_KEY+'/sendMessage?chat_id=' + user + '&text=' + msg
     rq.get(link)
 
