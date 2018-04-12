57b27eb4d6bf0ee85aa06dd4c998e08a7981b5c1
indionapolis <panddromas@gmail.com>
2018-04-12 20:35:46 +0300

    add new permissions

--- server/lmsinno/permissions.py
+++ server/lmsinno/permissions.py
@@ -1,6 +1,5 @@
 from django.utils.datastructures import MultiValueDictKeyError
 
-from rest_framework.views import exception_handler
 from rest_framework import permissions
 
 from .models import User
@@ -8,7 +7,6 @@ from .models import User
 from . import const
 
 
-# TODO new type of permissions
 class LibrarianPermission(permissions.BasePermission):
 
     def has_permission(self, request, view):
@@ -23,6 +21,13 @@ class LibrarianPermission(permissions.BasePermission):
                                                      const.LIBRARIAN3_ROLE):
             result = True
 
+        elif request.method == 'PATCH' and user.role == (const.LIBRARIAN_BASE_ROLE or
+                                                         const.LIBRARIAN1_ROLE or
+                                                         const.LIBRARIAN2_ROLE or
+                                                         const.LIBRARIAN3_ROLE):
+
+            result = True
+
         elif request.method == 'POST' and user.role == (const.LIBRARIAN_BASE_ROLE or
                                                         const.LIBRARIAN2_ROLE or
                                                         const.LIBRARIAN3_ROLE):
@@ -30,12 +35,6 @@ class LibrarianPermission(permissions.BasePermission):
 
         elif request.method == 'DELETE' and user.role == (const.LIBRARIAN_BASE_ROLE or
                                                           const.LIBRARIAN3_ROLE):
-            result = True
-
-        elif request.method == 'PATCH' and user.role == (const.LIBRARIAN_BASE_ROLE or
-                                                         const.LIBRARIAN1_ROLE or
-                                                         const.LIBRARIAN2_ROLE or
-                                                         const.LIBRARIAN3_ROLE):
             result = True
 
         else:
@@ -50,21 +49,21 @@ class UserPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         user = User.get_instance(request)
 
+        flag = user.pk == int(request.META['PATH_INFO'].split('/')[-1])
+
         if not user:
             return False
 
-        if request.method == 'GET' and (user.pk == int(request.META['PATH_INFO'].split('/')[-1])
-                                        or user.role == const.LIBRARIAN_BASE_ROLE):
+        if request.method == 'GET' and (flag or user.role == const.LIBRARIAN_BASE_ROLE):
             result = True
-        elif request.method == 'POST' and (user.pk == int(request.META['PATH_INFO'].split('/')[-1])
-                                           or user.role == const.LIBRARIAN_BASE_ROLE):
+        elif request.method == 'POST' and (flag or user.role == const.LIBRARIAN_BASE_ROLE):
             result = True
         elif request.method == 'DELETE' and user.role == const.LIBRARIAN_BASE_ROLE:
             result = True
         elif request.method == 'PATCH':
             if user.role == const.LIBRARIAN_BASE_ROLE:
                 result = True
-            elif user.pk == int(request.META['PATH_INFO'].split('/')[-1]):
+            elif flag:
                 result = True
                 try:
                     request.data['role']
@@ -77,43 +76,3 @@ class UserPermission(permissions.BasePermission):
             result = False
 
         return result
-
-
-class AuthenticatedUserPermission(permissions.BasePermission):
-
-    def has_permission(self, request, view):
-        user = User.get_instance(request)
-
-        if not user:
-            return False
-
-        if request.method == 'GET':
-            result = True
-        elif request.method == 'POST' and user.role == const.LIBRARIAN_BASE_ROLE:
-            result = True
-        elif request.method == 'PATCH' and user.role == const.LIBRARIAN_BASE_ROLE:
-            result = True
-        else:
-            result = False
-
-        return result
-
-
-def custom_exception_handler(exc, context):
-    # Call REST framework's default exception handler first,
-    # to get the standard error response.
-    response = exception_handler(exc, context)
-
-    data = {'status': '',
-            'data': {}}
-
-    # Now add the HTTP status code to the response.
-
-    if response is not None:
-        if response.status_code == 401:
-            data['status'] = 'HTTP_401_UNAUTHORIZED'
-        elif response.status_code == 403:
-            data['status'] = 'HTTP_403_FORBIDDEN'
-        response.data = data
-
-    return response
