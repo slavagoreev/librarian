036eab80198f097fb4f3d6dc7220c9051c3de261
indionapolis <panddromas@gmail.com>
2018-04-14 18:30:24 +0300

    perm impr

--- server/lmsinno/permissions.py
+++ server/lmsinno/permissions.py
@@ -12,13 +12,15 @@ def permission_0(fn):
         print('wrapper 0')
         if user.role not in const.LIBRARIAN_ROLE_LIST:
             if 'role' in request.data and request.data['role'] != user.role:
-                return Response(False, status=status.HTTP_403_FORBIDDEN)
+                message = {'message': 'you are trying to change your role'}
+                return Response(message, status=status.HTTP_403_FORBIDDEN)
 
             if 'user_id' in kwargs and int(kwargs['user_id']) != user.pk:
                 return Response(False, status=status.HTTP_403_FORBIDDEN)
         else:
-            if 'role' in request.data and request.data['role'] in const.LIBRARIAN_ROLE_LIST:
-                return Response(False, status=status.HTTP_403_FORBIDDEN)
+            if 'role' in request.data and int(request.data['role']) in const.LIBRARIAN_ROLE_LIST:
+                message = {'message': 'you are trying to change your role'}
+                return Response(message, status=status.HTTP_403_FORBIDDEN)
         return fn(request, *args, **kwargs)
     return wrapper
 
