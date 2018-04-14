180845328fbb9413bd5fbc165d51c06a5373dcdd
indionapolis <panddromas@gmail.com>
2018-04-14 18:22:14 +0300

    perm impr

--- server/lmsinno/permissions.py
+++ server/lmsinno/permissions.py
@@ -10,17 +10,15 @@ def permission_0(fn):
     def wrapper(request, *args, **kwargs):
         user = User.get_instance(request)
         print('wrapper 0')
-        print(request.data)
-        if user.role not in [const.LIBRARIAN_BASE_ROLE,
-                             const.LIBRARIAN1_ROLE,
-                             const.LIBRARIAN2_ROLE,
-                             const.LIBRARIAN3_ROLE]:
+        if user.role not in const.LIBRARIAN_ROLE_LIST:
             if 'role' in request.data and request.data['role'] != user.role:
                 return Response(False, status=status.HTTP_403_FORBIDDEN)
 
             if 'user_id' in kwargs and int(kwargs['user_id']) != user.pk:
                 return Response(False, status=status.HTTP_403_FORBIDDEN)
-
+        else:
+            if 'role' in request.data and request.data['role'] in const.LIBRARIAN_ROLE_LIST:
+                return Response(False, status=status.HTTP_403_FORBIDDEN)
         return fn(request, *args, **kwargs)
     return wrapper
 
@@ -29,10 +27,7 @@ def permission_1(fn):
     def wrapper(request, *args, **kwargs):
         user = User.get_instance(request)
         print('wrapper 1')
-        if user.role not in [const.LIBRARIAN_BASE_ROLE,
-                             const.LIBRARIAN1_ROLE,
-                             const.LIBRARIAN2_ROLE,
-                             const.LIBRARIAN3_ROLE]:
+        if user.role not in const.LIBRARIAN_ROLE_LIST:
             return Response(False, status=status.HTTP_403_FORBIDDEN)
         return fn(request, *args, **kwargs)
     return wrapper
