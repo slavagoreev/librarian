b00a1950b7b36ff57dbca1388a8e89927425669b
indionapolis <panddromas@gmail.com>
2018-04-19 21:04:52 +0300

    perf impr

--- server/lmsinno/permissions.py
+++ server/lmsinno/permissions.py
@@ -12,7 +12,6 @@ def permission_0(fn):
         if not user:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
         role_flag = 'role' in request.data and int(request.data['role']) != user.role
-        print('wrapper 0')
         if user.role not in const.LIBRARIAN_ROLE_LIST:
             if role_flag:
                 message = {'message': 'you are trying to change your role'}
@@ -33,7 +32,6 @@ def permission_1(fn):
         user = User.get_instance(request)
         if not user:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
-        print('wrapper 1')
         if user.role not in const.LIBRARIAN_ROLE_LIST:
             return Response(False, status=status.HTTP_403_FORBIDDEN)
         return fn(request, *args, **kwargs)
@@ -45,7 +43,6 @@ def permission_2(fn):
         user = User.get_instance(request)
         if not user:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
-        print('wrapper 2')
         if user.role not in [const.LIBRARIAN_BASE_ROLE,
                              const.LIBRARIAN2_ROLE,
                              const.LIBRARIAN3_ROLE]:
@@ -59,7 +56,6 @@ def permission_3(fn):
         user = User.get_instance(request)
         if not user:
             return Response(status=status.HTTP_401_UNAUTHORIZED)
-        print('wrapper 3')
         if user.role not in [const.LIBRARIAN_BASE_ROLE,
                              const.LIBRARIAN3_ROLE]:
             return Response(False, status=status.HTTP_403_FORBIDDEN)
